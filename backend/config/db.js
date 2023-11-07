4ef5e77eaeb3ccf7bcd122a26a325996977bf25f
dapdy <stfeleti@gmail.com>
2023-11-07 10:42:43 +0200

    changed db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI, {});
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
